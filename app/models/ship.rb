6e00e13c6d1e6a45688cce7b56c3401c5924904c
Cameron Hudson <camnn3@yahoo.com>
2020-09-21 20:23:17 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,7 +4,16 @@ class Ship
   
   def initialize(params)
     @name = params[:name]
-    @type = params[:name]
-    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
     @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all= []
+  end
 end
