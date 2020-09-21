898e60b5b03d864ad7c75a6546ade6f517754d19
Cameron Hudson <camnn3@yahoo.com>
2020-09-21 20:26:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+    get '/' do
+      erb :root
+    end
+    
+    get '/new' do
+      erb :"/pirates/new"
+    end
+    
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ship_1 = Ship.new(params[:pirate][:ships].first)
+      @ship_2 = Ship.new(params[:pirate][:ships].last)
+      
+      erb :"/pirates/show"
+    end
   end
 end
